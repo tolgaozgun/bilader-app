668d3f1e1c898c0c4ad83876a881fcda48e9f1d8
Tolga Özgün <tolgaozgunn@gmail.com>
2021-04-06 14:45:03 +0300

    Initial commit by tolgaozgun

---
+++ Android/app/src/main/java/com/breakdown/bilader/MainActivity.java
@@ -0,0 +1,13 @@
+package com.breakdown.bilader;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
