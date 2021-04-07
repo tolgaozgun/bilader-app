f08cde8f87cbfe49313819e9b435ba64a7b29969
Korhan Kemal Kaya <79211946+korhankemalkaya@users.noreply.github.com>
2021-04-07 23:05:14 +0300

    Product-Message-ProductList added

--- Android/app/src/main/java/com/breakdown/bilader/models/Message.java
+++ Android/app/src/main/java/com/breakdown/bilader/models/Message.java
@@ -1,4 +1,18 @@
 package com.breakdown.bilader.models;
 
 public class Message {
+
+    private Date messageDate;
+    private long messageTime;
+    private User sentBy;
+    private Date date;
+    private String content;
+
+    public Message(Date messageDate, long messageTime, User sentBy, Date date, String content) {
+        this.messageDate = messageDate;
+        this.messageTime = messageTime;
+        this.sentBy = sentBy;
+        this.date = date;
+        this.content = content;
+    }
 }
--- Android/app/src/main/java/com/breakdown/bilader/models/ProductList.java
+++ Android/app/src/main/java/com/breakdown/bilader/models/ProductList.java
@@ -1,4 +1,22 @@
 package com.breakdown.bilader.models;
 
 public class ProductList {
+
+    private List<Product> products;
+
+    public ProductList(List<Product> products) {
+        this.products = products;
+    }
+
+    public void addProduct(){
+        //ToDo
+    }
+
+    public void searchProduct(){
+        //ToDo
+    }
+
+    public void retrieveProducts(){
+        //ToDo
+    }
 }
