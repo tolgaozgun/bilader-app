b738dfeb35a6e1b171aa2886a6feac6a0abeaee1
tolgaozgun <tolgaozgunn@gmail.com>
2021-04-07 16:18:28 +0300

    Update Chat.java

--- Android/app/src/main/java/com/breakdown/bilader/models/Chat.java
+++ Android/app/src/main/java/com/breakdown/bilader/models/Chat.java
@@ -1,4 +1,20 @@
 package com.breakdown.bilader.models;
 
 public class Chat {
+    private User userSender;
+    private User userReceiver;
+    private final long id;
+    private String lastMessage;
+
+    public Chat(User userSender, User userReceiver, long id, String lastMessage){
+        this.id = id;
+        this.userReceiver = userReceiver;
+        this.userSender = userSender;
+        this.lastMessage = lastMessage;
+    }
+
+    public boolean sendMessage(Message message){
+        // TODO: Send message to the Websocket server
+        return false;
+    }
 }
