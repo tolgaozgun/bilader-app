79b90e0804f2f01a944d1367b2caf2aa1c9f58a3
BurcuKaplan <79203996+BurcuKaplan@users.noreply.github.com>
2021-04-07 21:11:15 +0300

    ChangedUser Class

--- Android/app/src/main/java/com/breakdown/bilader/models/User.java
+++ Android/app/src/main/java/com/breakdown/bilader/models/User.java
@@ -1,4 +1,56 @@
 package com.breakdown.bilader.models;
 
 public class User {
+    String userName;
+    String eMail;
+    String userAvatar;
+    ProductList userWishlist;
+    List<User> userFollowers;
+    List<User> userFollowings;
+    int reportNumber; //?
+    List<Product> userProducts;
+
+    public User( String userName, String eMail, String userAvatar){
+        this.userName = userName;
+        this.eMail = eMail;
+        this.userAvatar = userAvatar;
+
+        userWishlist = new ProductList();
+
+        userFollowers = new List<User>;
+        userFollowings = new List<User>;
+
+        userProducts = new List<Product>;
+
+        reportNumber = 0; //?
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String geteMail()
+    {
+        return eMail;
+    }
+
+    public String getUserAvatar()
+    {
+        return userAvatar;
+    }
+
+    public ProductList getWishlist()
+    {
+        return userWishlist;
+    }
+
+    public void addToFollowings( User user)
+    {
+        //TODO
+    }
+
+    public void report()
+    {
+        //TODO
+    }
 }
