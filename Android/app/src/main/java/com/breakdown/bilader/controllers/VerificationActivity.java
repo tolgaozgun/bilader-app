97f556fe3720d3fb828958d5e696184577003d42
Yeahya35 <yahyaeren35@hotmail.com>
2021-04-12 18:32:39 +0300

    registiration layout added

--- Android/app/src/main/java/com/breakdown/bilader/controllers/VerificationActivity.java
+++ Android/app/src/main/java/com/breakdown/bilader/controllers/VerificationActivity.java
@@ -2,5 +2,10 @@ package com.breakdown.bilader.controllers;
 
 import android.app.Activity;
 
-public class VerificationActivity extends Activity {
+public class VerificationActivity extends Activity
+{
+    /**
+     * Verification kısmında 4 digit sayı girilen yer için external library kullandım.
+     * Hangi commandleri olduğu burada yazılı: https://github.com/raycoarana/material-code-input
+     */
 }
