ae16fa7a2635a17b17baa7f1c3bf02d01761cb41
Yeahya35 <yahyaeren35@hotmail.com>
2021-04-12 18:41:09 +0300

    Revert "registiration layout added"
    This reverts commit 97f556fe3720d3fb828958d5e696184577003d42.

--- Android/app/src/main/java/com/breakdown/bilader/controllers/VerificationActivity.java
+++ Android/app/src/main/java/com/breakdown/bilader/controllers/VerificationActivity.java
@@ -2,10 +2,5 @@ package com.breakdown.bilader.controllers;
 
 import android.app.Activity;
 
-public class VerificationActivity extends Activity
-{
-    /**
-     * Verification kısmında 4 digit sayı girilen yer için external library kullandım.
-     * Hangi commandleri olduğu burada yazılı: https://github.com/raycoarana/material-code-input
-     */
+public class VerificationActivity extends Activity {
 }
