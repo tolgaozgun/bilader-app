fb381277a2c9a31cfbee6c3c1e3ad562789f23b2
denizgokcen1 <dnzgkcn25@gmail.com>
2021-04-13 12:39:27 +0300

    Bilkent space rules for register activity

--- Android/app/src/main/java/com/breakdown/bilader/controllers/RegisterActivity.java
+++ Android/app/src/main/java/com/breakdown/bilader/controllers/RegisterActivity.java
@@ -24,14 +24,14 @@ public class RegisterActivity extends Activity {
 
     @Override
     protected void onCreate( Bundle savedInstanceState ){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_register );
 
-        signUpRegisterButton = (Button) findViewById(R.id.button_sign_up_register);
-        inputName = (EditText) findViewById(R.id.editText_full_name);
-        inputEmail = (EditText) findViewById(R.id.editText_mail_register);
-        inputPasswordOnce = (EditText) findViewById(R.id.editText_password_register);
-        inputPasswordAgain = (EditText) findViewById(R.id.editText_password_again_register);
+        signUpRegisterButton = (Button) findViewById( R.id.button_sign_up_register );
+        inputName = (EditText) findViewById( R.id.editText_full_name );
+        inputEmail = (EditText) findViewById( R.id.editText_mail_register );
+        inputPasswordOnce = (EditText) findViewById( R.id.editText_password_register );
+        inputPasswordAgain = (EditText) findViewById(R.id.editText_password_again_register );
         loadingBar = new ProgressDialog(this );
 
         signUpRegisterButton.setOnClickListener( new View.OnClickListener() {
@@ -49,19 +49,19 @@ public class RegisterActivity extends Activity {
         String passwordTwo = inputPasswordAgain.getText().toString();
 
         if( TextUtils.isEmpty( name ) ){
-            Toast.makeText(this, "Please enter name!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter name!", Toast.LENGTH_SHORT ).show();
         }
         else if( TextUtils.isEmpty( email ) ){
-            Toast.makeText(this, "Please enter email!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter email!", Toast.LENGTH_SHORT ).show();
         }
         else if( TextUtils.isEmpty( passwordOne ) ){
-            Toast.makeText(this, "Please enter password!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter password!", Toast.LENGTH_SHORT ).show();
         }
         else if( TextUtils.isEmpty( passwordTwo ) ){
-            Toast.makeText(this, "Please enter password again!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter password again!", Toast.LENGTH_SHORT ).show();
         }
         else if( !passwordOne.equals( passwordTwo ) ){
-            Toast.makeText(this, "Passwords don't match!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Passwords don't match!", Toast.LENGTH_SHORT ).show();
         }
         else{
             loadingBar.setTitle( "sign up");
@@ -69,11 +69,11 @@ public class RegisterActivity extends Activity {
             loadingBar.setCanceledOnTouchOutside( false );
             loadingBar.show();
 
-            ValidateEmail(name, email, passwordOne, passwordTwo);
+            ValidateEmail( name, email, passwordOne, passwordTwo );
         }
     }
 
-    private void ValidateEmail(String name, final String email, String passwordOne, String passwordTwo) {
+    private void ValidateEmail( String name, final String email, String passwordOne, String passwordTwo ) {
         // TODO: Check if the email exists in the database.
         // TODO: If exists, return to login screen.
         // TODO: if the email is a bilkent email.
