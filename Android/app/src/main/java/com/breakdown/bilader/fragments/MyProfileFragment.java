383089fd5a5be09cc42534523f5f9dd093e555ac
denizgokcen1 <dnzgkcn25@gmail.com>
2021-04-20 16:18:28 +0300

    added log out method

--- Android/app/src/main/java/com/breakdown/bilader/fragments/MyProfileFragment.java
+++ Android/app/src/main/java/com/breakdown/bilader/fragments/MyProfileFragment.java
@@ -1,13 +1,17 @@
 package com.breakdown.bilader.fragments;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +20,7 @@ import androidx.fragment.app.Fragment;
 import com.breakdown.bilader.R;
 import com.breakdown.bilader.controllers.FollowersActivity;
 import com.breakdown.bilader.controllers.FollowingActivity;
+import com.breakdown.bilader.controllers.LoginActivity;
 import com.breakdown.bilader.controllers.MyProductsActivity;
 import com.breakdown.bilader.controllers.MyProfileActivity;
 import com.breakdown.bilader.controllers.OthersProfileActivity;
@@ -37,6 +42,7 @@ public class MyProfileFragment extends Fragment {
     private Button followingButton;
     private Button settingsButton;
     private Button logOutButton;
+    private ProgressDialog loadingBar;
 
     /**
      * Called to have the fragment instantiate its user interface view.
@@ -121,7 +127,7 @@ public class MyProfileFragment extends Fragment {
              * @param view is the view that was clicked
              */
             public void onClick( View view ) {
-                // TODO: log out
+                logOut();
             }
 
         } );
@@ -140,6 +146,18 @@ public class MyProfileFragment extends Fragment {
             }
 
         } );
+    }
+
+    private void logOut() {
+        loadingBar.setTitle( "log out" );
+        loadingBar.setMessage( "Logging out!" );
+        loadingBar.setCanceledOnTouchOutside( false );
+        loadingBar.show();
+
+        // TODO: log user out
+
+        Intent intent = new Intent( context, LoginActivity.class );
+        startActivity( intent );
     }
 
 }
