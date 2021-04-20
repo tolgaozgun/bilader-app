5f95b49a1ea498027beee5085f725a388e42fcad
denizgokcen1 <dnzgkcn25@gmail.com>
2021-04-20 16:47:55 +0300

    Added two switches and a button to settings

--- Android/app/src/main/java/com/breakdown/bilader/controllers/SettingsActivity.java
+++ Android/app/src/main/java/com/breakdown/bilader/controllers/SettingsActivity.java
@@ -1,6 +1,55 @@
 package com.breakdown.bilader.controllers;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.breakdown.bilader.R;
 
 public class SettingsActivity extends Activity {
+    private Button changePasswordButton;
+    private Switch followSwitch;
+    private Switch messageSwitch;
+
+    @Override
+    protected void onCreate( @Nullable Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_settings );
+
+        changePasswordButton = ( Button ) findViewById( R.id.buttonLogIn );
+        followSwitch = ( Switch ) findViewById( R.id.switch_follow_notifications );
+        messageSwitch = ( Switch ) findViewById( R.id.switch_message_notifications );
+
+        changePasswordButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                Intent intent;
+                intent = new Intent( SettingsActivity.this,
+                        ChangePasswordActivity.class );
+                startActivity( intent );
+            }
+        } );
+
+        followSwitch.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                // TODO
+            }
+        } );
+
+        messageSwitch.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                // TODO
+            }
+        } );
+    }
 }
